4cd2d2e1281d3a5fbae8c32860ba6f0aaa341718
Krishna <surekrrishna@gmail.com>
2021-03-03 19:23:50 +0530

    function
    created employee function to get his details

--- postgres.sql
+++ postgres.sql
@@ -1,2 +1,21 @@
 -- below query will get the current month and date plus one month including date, for eg:- 23-03 to 23-04
 select * from employee where to_char(dob, 'MMDD') between to_char(current_date, 'MMDD') and to_char(current_date + 30, 'MMDD')
+
+-- created a function to get the employee details
+CREATE OR REPLACE FUNCTION emp_details (employee_id bigint) 
+   RETURNS TABLE (
+	  emp_id bigint,
+	  emp_first_name varchar(50),
+	  emp_status varchar(20)
+) 
+LANGUAGE plpgsql
+AS
+$func$
+
+BEGIN
+RETURN QUERY SELECT id AS emp_id, first_name AS emp_first_name, emp_status AS status FROM employee where id = employee_id;
+END; 
+
+$func$
+
+select emp_details(1)
